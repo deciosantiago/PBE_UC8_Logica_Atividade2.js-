6e4bb14d29369547c23f347bb4c0a4c6bec71a6d
worm <deciosantiago.ds@gmail.com>
2021-12-12 03:08:35 -0300

    alteracao atividade

--- PBE_UC8_Logica_Atividade2.js
+++ PBE_UC8_Logica_Atividade2.js
@@ -6,16 +6,35 @@ if (ListaDePecas.length<10) {
     console.log("Não é possível cadastrar mais peças")
 }
 
-let peso = 50;
-if(peso<100){
+let Peso = 50;
+if(Peso<100){
     console.log("A peça deve pesar no mínimo 100g")
 }else{
     console.log("A peça possui peso adequado")
 }
 
-let nomedapeca = "filtro de ar"
-if(nomedapeca.length>3){
+let NomeDaPeca = "filtro de ar"
+if(NomeDaPeca.length>3){
     console.log("Nome da peça adequado para cadastro")
 }else{
     console.log("O nome deve ter mais de 3 caracteres")
+}
+if(NomeDaPeca.length ==0) {
+        console.log("O nome da peça não pode ser vazio")
+    } else {
+        console.log("O nome deve ter mais de 3 caracteres, digite um nome adequado")
+    }
+
+
+switch(NomeDaPeca.length) {
+    case 0:
+        console.log("O nome deve ter 3 ou mais caracteres. Digite novamente")
+    case 1:
+    
+    case 2:
+    
+    case 3:
+
+    default:
+        console.log("Nome da peça adequado para o cadastro!")
 }
